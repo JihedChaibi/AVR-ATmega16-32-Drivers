0d61ad494224faf2035210d44e834f118bdc003d
JihedChaibi <jihed.chaibi.1995@gmail.com>
2020-06-30 10:36:35 +0200

    update AVR_MACROS.h

--- inc/AVR_MACROS.h
+++ inc/AVR_MACROS.h
@@ -10,10 +10,12 @@
 #ifndef AVR_MACROS_H_
 #define AVR_MACROS_H_
 
+
 #include <stdint.h>
 
-#include <avr/io.h>
 #define F_CPU 8000000UL
+
+#include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
