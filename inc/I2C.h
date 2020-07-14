b07ebd3f5d1973ad1dd081724be9e409b6d4e67a
JihedChaibi <jihed.chaibi.1995@gmail.com>
2020-07-14 14:51:19 +0200

    update I2C drivers

--- inc/I2C.h
+++ inc/I2C.h
@@ -6,3 +6,23 @@
 *  Description:   Basic I2C Functions Declarations
 *
 */
+
+#ifndef  I2C_AVR_H_
+#define  I2C_AVR_H_
+
+#include "AVR_MACROS.h"
+
+void    I2C_master_init(void);
+void    I2C_start(void);
+void    I2C_write(uint8_t data);
+uint8_t I2C_read(void);
+void    I2C_stop(void);
+uint8_t I2C_slave_available(void);
+void    I2C_slave_init();
+
+
+
+
+
+
+#endif
--- src/I2C.c
+++ src/I2C.c
@@ -8,3 +8,30 @@
 */
 
 #include "I2C.h"
+
+#define I2C_PRESCALER 1
+#define F_SCL 4000000
+#define TWBR_Value (((F_CPU/F_SCL)-16)/(2*I2C_PRESCALER))
+
+void    I2C_master_init(void)
+{
+
+TWBR = (uint8_t)TWBR_Value;
+
+}
+
+
+void    I2C_start(void)
+{
+
+TWCR = 0;
+TWCR |= ((1 << TWEN) | (1 << TWSTA) | (1 << TWINT));   
+
+}
+
+
+void    I2C_write(uint8_t data);
+uint8_t I2C_read(void);
+void    I2C_stop(void);
+uint8_t I2C_slave_available(void);
+void    I2C_slave_init();
